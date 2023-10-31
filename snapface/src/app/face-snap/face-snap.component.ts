e48868ff3b4ca480948b51851dca871b97ea5801
Thomas <thomas.meyet@epitech.eu>
2023-10-31 16:35:32 +0100

    COMPONENT Start the project with this component

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+
+  ngOnInit(): void {
+    this.title= "Début sur Angular";
+    this.description= "J'espère que c'est bien !";
+    this.createDate= new Date();
+    this.snaps = 1;
+  }
 
 }
