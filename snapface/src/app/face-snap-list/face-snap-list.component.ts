7609c6802cabbc700cac22f0af1da0a0aa82c502
Thomas <thomas.meyet@epitech.eu>
2023-11-16 11:21:33 +0100

    HEADER Add the header and start the service

--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snaps.models';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -8,41 +9,10 @@ import { FaceSnap } from '../models/face-snaps.models';
 })
 export class FaceSnapListComponent implements OnInit {
   faceSnap!: FaceSnap[];
-  constructor () {}
+  constructor (private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit(): void {
-    this.faceSnap = [
-      {
-        title: 'Début sur Angular',
-        desciption: "J'espère que c'est bien !",
-        imgUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-        createDate: new Date(),
-        snaps: 150,
-        location: "Lyon",
-      },
-      {
-        title: 'Réutilisation du component',
-        desciption: "J'espere que cela va marcher 🤞",
-        imgUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-        createDate: new Date(),
-        snaps: 0,
-        location: "le jardin"
-      },
-      {
-        title: '3 images identiques',
-        desciption: "Je sais il y a trois images identiques mais laquelles est la vrai ?",
-        imgUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-        createDate: new Date(),
-        snaps: 0
-      },
-      {
-        title: '3 images identiques',
-        desciption: "Je sais il y a trois images identiques mais laquelles est la vrai ?",
-        imgUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-        createDate: new Date(),
-        snaps: 0
-      }
-    ]
+    this.faceSnap = this.faceSnapsService.faceSnaps;
   }
 
 }
